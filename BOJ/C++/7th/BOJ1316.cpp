8792f9d947ed3994a7f90d16c089a13c62810b14
ymiwm <ymiwm0322@gmail.com>
2021-02-24 16:52:00 +0900

    BOJ1316 update

---
+++ BOJ/C++/7th/BOJ1316.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+int main(void)
+{
+    int N;
+    int count = 0;
+    std::cin >> N;
+    std::string str;
+    std::vector<bool> flag(26, false);
+    for(int i = 0; i < N; i++)
+    {
+        std::cin >> str;
+        for(int j = 0; j < str.size(); j++)
+        {
+            if(flag.at(str.at(j) - 'a') == false)
+            {
+                for(int k = j; k < str.size(); k++)
+                {
+                    if(str.at(j) == str.at(k))
+                        continue;
+                    else
+                    {
+                        flag.at(str.at(j) - 'a') = true;
+                        j = k - 1;
+                        break;
+                    }
+                }
+            }
+            else
+                break;
+            if(j == str.size() - 1)
+                count++;
+        }
+        flag.assign(26, false);
+    }
+    std::cout << count;
+}
